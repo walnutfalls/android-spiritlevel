437ce8076945d865b278ff37bbd515d2e846e1df
concubicycle <concubicyclestuffsim@gmail.com>
2015-07-07 22:50:04 -0400

    first

---
+++ app/src/main/java/com/footbits/sava/oglspiritleveldisplay/SpiritLevelActivityFragment.java
@@ -0,0 +1,23 @@
+package com.footbits.sava.oglspiritleveldisplay;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SpiritLevelActivityFragment extends Fragment {
+
+    public SpiritLevelActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_spirit_level, container, false);
+    }
+}
---
+++ fractoustext/src/main/java/com/footbits/Font.java
@@ -0,0 +1,4 @@
+package com.footbits;
+
+public class Font {
+}
