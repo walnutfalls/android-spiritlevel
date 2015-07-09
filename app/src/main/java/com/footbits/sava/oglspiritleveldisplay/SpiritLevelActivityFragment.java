1b97823589dfa243f30fdd208a8e80bb9c3d83dc
concubicycle <concubicyclestuffsim@gmail.com>
2015-07-08 23:31:42 -0400

    added android orientation sensor lib

--- app/src/main/java/com/footbits/sava/oglspiritleveldisplay/SpiritLevelActivityFragment.java
+++ app/src/main/java/com/footbits/sava/oglspiritleveldisplay/SpiritLevelActivityFragment.java
@@ -6,15 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.texample2.GLText;
+import com.golshadi.orientationSensor.sensors.Orientation;
+import com.golshadi.orientationSensor.utils.OrientationSensorInterface;
 
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class SpiritLevelActivityFragment extends Fragment {
+public class SpiritLevelActivityFragment extends Fragment implements OrientationSensorInterface {
 
     MyGLSurfaceView sampleView;
+    Orientation orientationSensor;
+
 
     public SpiritLevelActivityFragment() {
     }
@@ -24,8 +24,26 @@ public class SpiritLevelActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         sampleView = new MyGLSurfaceView(this.getActivity());
+        orientationSensor = new Orientation(getActivity().getBaseContext(), this);
+
+        //------Turn Orientation sensor ON-------
+        // set tolerance for any directions
+        orientationSensor.init(1.0, 1.0, 1.0);
+
+        // set output speed and turn initialized sensor on
+        // 0 Normal
+        // 1 UI
+        // 2 GAME
+        // 3 FASTEST
+        orientationSensor.on(3);
+
         return sampleView; // inflater.inflate(R.layout.fragment_spirit_level, container, false);
 
+
+    }
+
+    @Override
+    public void orientation(Double AZIMUTH, Double PITCH, Double ROLL) {
 
     }
 }
