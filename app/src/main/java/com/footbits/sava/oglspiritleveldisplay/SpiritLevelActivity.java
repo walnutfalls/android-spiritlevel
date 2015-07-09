365bd7a32c71faccf9561259f1d24fc471a15308
concubicycle <concubicyclestuffsim@gmail.com>
2015-07-08 22:06:12 -0400

    shifting libs

--- app/src/main/java/com/footbits/sava/oglspiritleveldisplay/SpiritLevelActivity.java
+++ app/src/main/java/com/footbits/sava/oglspiritleveldisplay/SpiritLevelActivity.java
@@ -1,7 +1,6 @@
 package com.footbits.sava.oglspiritleveldisplay;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
