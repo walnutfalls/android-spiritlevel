02424f1b83e03bc61f9206b4d21708a213d61b73
concubicycle <concubicyclestuffsim@gmail.com>
2015-08-20 23:36:15 -0400

    added scale gesture listener

--- app/src/main/java/com/footbits/sava/oglspiritleveldisplay/AngleViewSurface.java
+++ app/src/main/java/com/footbits/sava/oglspiritleveldisplay/AngleViewSurface.java
@@ -1,22 +1,55 @@
 package com.footbits.sava.oglspiritleveldisplay;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.opengl.GLSurfaceView;
+import android.support.v4.view.ScaleGestureDetectorCompat;
+import android.support.v4.view.ViewCompat;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 
 
 public class AngleViewSurface extends GLSurfaceView {
 
     IAngleProvider angleProvider;
     IAngleListener anglesArrivedResponder;
+    private ScaleGestureDetector scaleGestureDetector;
+
+    /**
+     * The scale listener, used for handling multi-finger scale gestures.
+     */
+    private final ScaleGestureDetector.OnScaleGestureListener mScaleGestureListener
+            = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        /**
+         * This is the active focal point in terms of the viewport. Could be a local
+         * variable but kept here to minimize per-frame allocations.
+         */
+
+        private float lastSpanX;
+        private float lastSpanY;
+
+        // Detects that new pointers are going down.
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+            return true;
+        }
+
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            float focusX = scaleGestureDetector.getFocusX();
+            float focusY = scaleGestureDetector.getFocusY();
+            return true;
+        }
+    };
 
 
     public AngleViewSurface(Context context, final AngleGLRenderer renderer){
         super(context);
 
-
         angleProvider = new AndroidOrientationSensorAngleProvider(context);
         anglesArrivedResponder = new SpiritLevelAngleListener(context, renderer, this);
+        scaleGestureDetector = new ScaleGestureDetector(context, mScaleGestureListener);
         angleProvider.getAngleListeners().add(anglesArrivedResponder);
 
 
@@ -39,8 +72,12 @@ public class AngleViewSurface extends GLSurfaceView {
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
+        boolean retVal = scaleGestureDetector.onTouchEvent(e);
+        retVal = scaleGestureDetector.onTouchEvent(e) || retVal;
+
         angleProvider.calibrate();
         anglesArrivedResponder.anglesArrived(0, 0);
-        return true;
+
+        return retVal || super.onTouchEvent(e);
     }
 }
